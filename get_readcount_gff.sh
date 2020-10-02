09b68e87d8e94773bb827dac0d2494f10782ce53
RickGelhausen <rick.gelhausen@gmail.com>
2020-10-02 14:33:50 +0000

    added new scripts for calcultation of expression

---
+++ get_readcount_gff.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+source activate featurecounting
+
+bam_path="/mnt/datavault/SPP2002/analysis/exp28/bam"
+input_annotation=""
+output_rawreads=""
+output_annotation=""
+output_mapped=""
+output_length=""
+
+# Handling input
+while getopts "h?i:o:b:r:m:l:" opt; do
+    case "$opt" in
+    h|\?)
+        exit 0
+        ;;
+    b)  bam_path=$OPTARG
+        ;;
+    i)  input_annotation=$OPTARG
+        ;;
+    r)  output_rawreads=$OPTARG
+        ;;
+    m)  output_mapped=$OPTARG
+        ;;
+    l)  output_length=$OPTARG
+        ;;
+    o)  output_annotation=$OPTARG
+        ;;
+    esac
+done
+
+bam_list=()
+for file in $bam_path/*.bam; do
+    bam_list+=("$file")
+done
+
+echo "${bam_list[@]}"
+echo "$output_annotation"
+echo "$input_annotation"
+
+python3 total_mapped_reads.py -b "${bam_list[@]}" -m "$output_mapped" -l "$output_length"
+python3 call_featurecounts.py -b "${bam_list[@]}" -s 1 --with_O -o "$output_rawreads" -t 8 -a "$input_annotation"
+python3 map_reads_to_annotation.py -i "$output_rawreads" -a "$input_annotation" -o "$output_annotation"
